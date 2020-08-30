9bb1a83ad6f703813bed8e361f191684c7d9775d
nathan <nathan.chalot@gmail.com>
2020-08-30 14:44:44 +0200

    Use events data in EventLite component

--- app/javascript/packs/components/EventLite.jsx
+++ app/javascript/packs/components/EventLite.jsx
@@ -2,16 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class EventLite extends React.Component {
+  /*
+  ** To me loading events in state after getting them through
+  ** props is weird : either a bad practice or simply ugly ?
+  ** TODO => findout && do it another way
+  */
+  state = {
+    events: this.props.events
+  };
+
   render() {
     return (
-      <h2>COUCOU!</h2>
-    )
+      <div>
+        { this.state.events.map((event) => {
+          return (
+            <p>{event.title}</p>
+          );
+        })}
+      </div>
+    );
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  const dataNode = document.getElementById('events-data');
+  const events = JSON.parse(dataNode.getAttribute('data-events'));
   ReactDOM.render(
-    <EventLite />,
+    <EventLite events={events}/>,
     document.body.appendChild(document.createElement('div')),
   )
 })
