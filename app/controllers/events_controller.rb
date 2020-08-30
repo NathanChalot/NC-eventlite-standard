0dcebc537b81c0f2c391df738b02eff753ce5b96
nathan <nathan.chalot@gmail.com>
2020-08-30 15:30:01 +0200

    Select only needed fields in EventsController#index and order by start asc

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.select(:id, :title, :start_datetime, :location)
+      .order(start_datetime: :asc)
   end
 end
